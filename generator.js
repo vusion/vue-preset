910201a34af23e12eae8999e3da7dcc8cdcb45f2
zhangsanshi <zhangsanshi5566@gmail.com>
2020-01-10 14:28:16 +0800

    support install

--- generator.js
+++ generator.js
@@ -3,9 +3,18 @@ console.log(process.cwd());
 module.exports = (api, options, rootOptions) => {
     console.log(options, rootOptions);
     api.onCreateComplete(() => {
-        cp.spawn('npm', `create vusion ${options.templateType} ${rootOptions.projectName} -f`.split(' '), {
+        const create = cp.spawn('npm', `create vusion ${options.templateType} ${rootOptions.projectName} -f`.split(' '), {
             cwd: process.cwd(),
             stdio: 'inherit',
+        });
+        create.on('close', (code) => {
+            if (code !== 0) {
+              console.log(`ps process exited with code ${code}`);
+            }
+            cp.spawn('npm', `i`.split(' '), {
+                cwd: process.cwd(),
+                stdio: 'inherit',
+            });
         });
     });
 };
