2efe8c8667f3bb19a4c5853da6101c4291065a4c
zhangsanshi <zhangsanshi5566@gmail.com>
2020-01-10 14:40:10 +0800

    unsupport install

--- generator.js
+++ generator.js
@@ -3,18 +3,20 @@ console.log(process.cwd());
 module.exports = (api, options, rootOptions) => {
     console.log(options, rootOptions);
     api.onCreateComplete(() => {
-        const create = cp.spawn('npm', `create vusion ${options.templateType} ${rootOptions.projectName} -f`.split(' '), {
-            cwd: process.cwd(),
-            stdio: 'inherit',
-        });
-        create.on('close', (code) => {
-            if (code !== 0) {
-              console.log(`ps process exited with code ${code}`);
-            }
-            cp.spawn('npm', `i`.split(' '), {
+        setTimeout(() => {
+            const create = cp.spawn('npm', `create vusion ${options.templateType} ${rootOptions.projectName} -f`.split(' '), {
                 cwd: process.cwd(),
                 stdio: 'inherit',
             });
-        });
+            create.on('close', (code) => {
+                if (code !== 0) {
+                  console.log(`ps process exited with code ${code}`);
+                }
+                cp.spawn('npm', `i`.split(' '), {
+                    cwd: process.cwd(),
+                    stdio: 'inherit',
+                });
+            });
+        }, 1000);
     });
 };
