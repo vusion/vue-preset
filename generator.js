ae324cb8c33b4fb17087fbfe488d0d970ffbb14d
zhangsanshi <zhangsanshi5566@gmail.com>
2020-01-10 16:58:40 +0800

    fix print log

--- generator.js
+++ generator.js
@@ -2,6 +2,7 @@ const cp = require('child_process');
 const path = require('path');
 module.exports = (api, options, rootOptions) => {
     api.onCreateComplete(() => {
+        console.log('install cloud-admin ...');
         const create = cp.spawn('npm', `create vusion cloud-admin-lite ${rootOptions.projectName} -f`.split(' '), {
             cwd: process.cwd(),
         });
@@ -10,9 +11,9 @@ module.exports = (api, options, rootOptions) => {
                 console.error('npm create vusion exited with code' + code);
                 return;
             }
+            console.log('install cloud-admin dependencies ...');
             cp.spawn(api.generator.pm || 'npm', ['install'], {
                 cwd: path.join(process.cwd(), rootOptions.projectName),
-                stdio: 'inherit',
             });
         });
     });
